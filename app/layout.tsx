5b7cdb267c3dcb0ba34dfc52ed23264b823fd65f
nipx <36599403+nipx@users.noreply.github.com>
2023-12-13 16:46:45 -0500

    fix(warning): Replace 'dark' class on html tag by the property 'suppressHydrationWarning'

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
